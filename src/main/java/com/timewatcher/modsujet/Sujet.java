b4bfa200325cb1a941a51dac8451c5ad6c07b0ac
timewatcher659 <timewatcher46@gmail.com>
2025-10-15 23:04:05 +0300

    Update Sujet.java

--- src/main/java/com/timewatcher/modsujet/Sujet.java
+++ src/main/java/com/timewatcher/modsujet/Sujet.java
@@ -1,9 +1,14 @@
+package com.timewatcher.modsujet;
 
+import net.minecraftforge.fml.common.Mod;
+import com.timewatcher.modsujet.registry.ModItems;
+
+@Mod(SwordMod.MOD_ID)
 public class SwordMod {
     public static final String MOD_ID = "mod_sujet_phone";
 
     public SwordMod() {
-        
+        // Вызываем регистрацию нашего меча при запуске мода
+        ModItems.register(); 
     }
 }
-
